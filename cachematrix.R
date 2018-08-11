e01115a9881e2ef8e5a2db2633c7df3f1c814e26
Rpmmat <40347811+Rpmmat@users.noreply.github.com>
2018-08-12 00:03:45 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+
+## In makeCacheMatrix function it creates a matrix to cache the inverse of a matrix
+## We will inverse the matrix in the cahceSolve function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  
+  inv = NULL
+  set = function(y) {
+    
+    x <<- y
+    inv <<- NULL
+  }
+  get = function() x
+  setinv = function(inverse) inv <<- inverse 
+  getinv = function() inv
+  list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
 
-
-## Write a short comment describing this function
-
+## the following function find the inverse of matrix x using the inbuilt R solve function
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+ 
+  inv = x$getinv()
+  
+
+  if (!is.null(inv)){
+    
+    message("getting cached data")
+    return(inv)
+  }
+  
+
+  mat.data = x$get()
+  inv = solve(mat.data, ...)
+
+  x$setinv(inv)
+  
+  return(inv)
 }
